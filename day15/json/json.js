0de7037531d7c09e830c0c3de3f25af06a6abac0
DinoEgg-code <dinoegg92@gmail.com>
2026-03-20 17:12:06 +0900

    day16 과제 끝

--- day15/json/json.js
+++ day15/json/json.js
@@ -1,4 +1,19 @@
-const jsonString = '{"name": "John", "age": 30 }';
+const wrongJsonString = `{
+    "name": "John",
+    "age": 30,
+}`;
+try {
+  const jsonObj = JSON.parse(wrongJsonString);
+  const { name: objName, age: objAge } = jsonObj;
+  console.log(objName, objAge);
+} catch (error) {
+  console.log("에러:", error);
+}
+
+const jsonString = `{
+    "name": "John",
+    "age": 30
+}`;
 console.log(jsonString);
 
 const jsonObject = JSON.parse(jsonString);
@@ -7,18 +22,25 @@ console.log(jsonObject);
 const jsonStringified = JSON.stringify(jsonObject);
 console.log(jsonStringified);
 
-
-
 // const age = jsonObject.age;
 // const name = jsonObject.name;
 const { age, name } = jsonObject;
-console.log(age);
-console.log(name);
+console.log(age, name);
 
+// const myName = jsonObject.name;
+// const myAge = jsonObject.age;
 const { name: myName, age: myAge } = jsonObject;
-console.log(myName);
-console.log(myAge);
+console.log(myName, myAge);
 
-const jsonStringArr = '[{"name": "John", "age": 30 }, {"name": "Jane", "age": 25 }]';
+const jsonStringArr = `[
+    {
+        "name": "John",
+        "age": 30
+    },
+    {
+        "name": "Jane",
+        "age": 25
+    }
+]`;
 const jsonArray = JSON.parse(jsonStringArr);
 console.log(jsonArray);
